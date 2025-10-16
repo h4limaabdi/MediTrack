3657650f959b926cb49e529a07c1d809ffbb3c3c
h4lima <h4limaabdi@gmail.com>
2025-10-16 23:38:39 +0300

    Style the page

---
+++ Registeration/registration.js
@@ -0,0 +1,6 @@
+const medForm = document.getElementById("form");
+const addbtn = document.getElementById("add");
+const submitBtn = document.getElementById("submit");
+
+//submit event listener
+submitBtn.addEventListener
