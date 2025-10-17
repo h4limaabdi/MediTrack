03b093ddb725d5196d7f021e579d70b98d01ca5f
h4lima <h4limaabdi@gmail.com>
2025-10-17 13:07:27 +0300

    Push all files to GitHub

--- Dashboard/index.js
+++ Dashboard/index.js
@@ -7,13 +7,13 @@ if (savedMeds.length === 0){
         const startBtn = document.createElement("button");
         startBtn.textContent = "Start Now!";
         startBtn.addEventListener("click", () => {
-             window.location.href = "Registration.html";
+             window.location.href = "../Registeration/Registration.html";
         });
         medList.appendChild(startBtn);
 } else {
     savedMeds.forEach((med) => {
         const li = document.createElement("li");
-        li textContent = `${med.medName}\n${med.dosage}\n${med.time}`;
+        li.textContent = `${med.medName}\n${med.dosage}\n${med.time}`;
         medList.appendChild(li);
     });
 }
