8724bef771fc04597a24d7f991db8630b7a1d54c
h4lima <h4limaabdi@gmail.com>
2025-10-17 05:41:18 +0300

    Maintain header/foote

--- Registeration/registration.js
+++ Registeration/registration.js
@@ -1,6 +1,26 @@
+alert("This is a prototype!");
+
 const medForm = document.getElementById("form");
 const addbtn = document.getElementById("add");
 const submitBtn = document.getElementById("submit");
 
-//submit event listener
-submitBtn.addEventListener
+let meds = [];
+
+addbtn.addEventListener("click", (event) =>{
+    const medName = document.getElementById("medName").value;
+    const dosage = document.getElementById("dosage").value;
+    const  time = document.getElementById("time").value;
+
+    if (medName && dosage && time){
+        const med = {medName, dosage, time};
+        meds.push(med);
+        alert(`Added: ${medName}  ${dosage} ${time}`);
+    } else {
+        alert("Fill in all the fields");
+    }
+});
+
+submitBtn.addEventListener("click", (event) =>{
+    localStorage.setItem("meds", JSON.stringify(meds));
+    window.location.href = "Dashboard/index.html";
+});
